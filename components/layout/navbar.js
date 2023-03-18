301736a28c87786a166b9d7674f57a91bde3b972
Kyle Wong <Kwong498@gmail.com>
2023-03-18 13:53:58 -0400

    basic auth, navbar and pages created

---
+++ components/layout/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { userRouter } from 'next/router';
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+	return (
+		<div className={styles.nav}>
+			<Link href="/">Home</Link>
+			<Link href="/news">News and Updates</Link>
+			<Link href="/about">About</Link>
+			<Link href="/how-to-play">How to Play</Link>
+			<Link href="/collection">Cards</Link>
+			<Link href="/user">Account</Link>
+			<Link href="/login">Login/Sign Up</Link>
+		</div>
+	);
+};
+
+export default Navbar;
