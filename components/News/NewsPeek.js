6bdf40290fb24d479e3b252d6e8c9791253bbdb6
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-03-18 16:30:35 -0500

    added some motions and fixed some text content

--- components/News/NewsPeek.js
+++ components/News/NewsPeek.js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import classes from "./NewsPeek.module.css";
+import { motion } from "framer-motion";
+import container from "./newsvariants";
 
 const NewsPeek = ({ article }) => {
   const router = useRouter();
@@ -9,10 +11,20 @@ const NewsPeek = ({ article }) => {
   };
 
   return (
-    <div onClick={newsClick} className={classes.newscontainer}>
+    <motion.div
+      whileHover={{
+        boxShadow:
+          "rgba(246, 170, 28, 0.10) 0px 54px 55px, rgba(246, 170, 28, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(246, 170, 28, 0.1) 0px 12px 13px, rgba(246, 170, 28, 0.1) 0px -3px 5px",
+        scale: 1.05,
+        height: 300,
+      }}
+      onClick={newsClick}
+      className={classes.newscontainer}
+    >
       <h1>{article.heading}</h1>
       <p>{article.author}</p>
-    </div>
+      <h3 className={classes.expanded}>{article.tagline}</h3>
+    </motion.div>
   );
 };
 
