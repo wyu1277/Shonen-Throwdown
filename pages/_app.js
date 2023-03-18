e58facaef8d7e64e15ebaf419008786d23e5f959
Kyle Wong <Kwong498@gmail.com>
2023-03-18 19:23:28 -0400

    dynamic search bar working

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,18 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { useState } from 'react';
 import '@/styles/globals.css';
 import Navbar from '../components/layout/navbar';
+import { Provider } from 'react-redux';
+import store from '@/reducers/store';
 
 export default function App({ Component, pageProps }) {
 	const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
 	return (
 		<SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-			<Navbar />
-			<Component {...pageProps} />
+			<Provider store={store}>
+				<Navbar />
+				<Component {...pageProps} />
+			</Provider>
 		</SessionContextProvider>
 	);
 }
--- pages/collection/index.js
+++ pages/collection/index.js
@@ -3,12 +3,12 @@
 //! INSTALL WHEN NEEDED
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useEffect, useState } from 'react';
+import styles from './Collection.module.css';
 
 const Collection = () => {
 	const supabase = useSupabaseClient();
-	// const user = useUser();
-	//! user to check if logged in
 	const [data, setData] = useState();
+	const [searchInput, setSearchInput] = useState('');
 
 	useEffect(() => {
 		const loadData = async () => {
@@ -18,17 +18,31 @@ const Collection = () => {
 		loadData();
 	}, []);
 
+	const filteredData = data && data.filter((card) => card.name.toLowerCase().includes(searchInput.toLowerCase()));
+
 	return (
 		<div>
-			{data
-				? data.map((card) => (
-						<div>
-							<p>name: {card.name}</p>
-							<p>power: {card.power}</p>
-							<p>element: {card.element}</p>
-						</div>
-				  ))
-				: 'There are no cards'}
+			<div>
+				<input
+					className={styles.searchBar}
+					type="text"
+					value={searchInput}
+					onChange={(e) => setSearchInput(e.target.value)}
+					placeholder="Search by name..."
+				/>
+			</div>
+			<div className={styles.cardParent}>
+				{filteredData !== undefined
+					? filteredData.map((card) => (
+							<div className={styles.card} key={card.id}>
+								{/* REPLACE WITH CARD IMAGE LATER */}
+								<p>name: {card.name}</p>
+								<p>power: {card.power}</p>
+								<p>element: {card.element}</p>
+							</div>
+					  ))
+					: 'There are no cards'}
+			</div>
 		</div>
 	);
 };
