b40d3dadddbc7b25c595b3a8edd285458984fee1
jon <jkwei.86@gmail.com>
2023-03-19 19:49:21 -0400

    about page basics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { useState } from 'react';
-import '@/styles/globals.css';
-import Navbar from '../components/layout/navbar';
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import "@/styles/globals.css";
+import Navbar from "../components/layout/navbar";
 
 export default function App({ Component, pageProps }) {
-	const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
-	return (
-		<SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-			<Navbar />
-			<Component {...pageProps} />
-		</SessionContextProvider>
-	);
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Navbar />
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
