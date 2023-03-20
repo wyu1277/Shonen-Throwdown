a5e27b5d57ac6bc7c4e86f1a1c84e19aa8e1c059
jon <jkwei.86@gmail.com>
2023-03-20 14:02:24 -0400

    fixed github branches, about page, game page added to start testing real-time with supabase

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { userRouter } from 'next/router';
-import Link from 'next/link';
-import styles from './Navbar.module.css';
+import React from "react";
+import { userRouter } from "next/router";
+import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-	return (
-		<div className={styles.nav}>
-			<Link href="/">Home</Link>
-			<Link href="/news">News and Updates</Link>
-			<Link href="/about">About</Link>
-			<Link href="/how-to-play">How to Play</Link>
-			<Link href="/collection">Cards</Link>
-			<Link href="/user">Account</Link>
-			<Link href="/login">Login/Sign Up</Link>
-		</div>
-	);
+  return (
+    <div className={styles.nav}>
+      <Link href="/">Home</Link>
+      <Link href="/news">News and Updates</Link>
+      <Link href="/about">About</Link>
+      <Link href="/how-to-play">How to Play</Link>
+      <Link href="/collection">Cards</Link>
+      <Link href="/user">Account</Link>
+      <Link href="/login">Login/Sign Up</Link>
+      <Link href="/game">Game</Link>
+    </div>
+  );
 };
 
 export default Navbar;
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-	return (
-		<div>
-			<h1>ABOUT</h1>
-		</div>
-	);
+  const team = [
+    {
+      img: "",
+      name: "Kyle Wong",
+      email: "kwong498@gmail.com",
+      bio: "loremipsumloreamipsum",
+      linkedIn: "kylematthewwong",
+      github: "Kywongster",
+    },
+    {
+      img: "",
+      name: "Victor Flores",
+      email: "thelegendofvictor@gmail.com",
+      bio: "loremipsumloreamipsum",
+      linkedIn: "mikloflores",
+      github: "Miklo1775",
+    },
+    {
+      img: "",
+      name: "Wilson Yu",
+      email: "wyu1277@gmail.com",
+      bio: "loremipsumloreamipsum",
+      linkedIn: "whydesigns",
+    },
+    {
+      img: "",
+      name: "Jonathan Wei",
+      email: "jkwei.86@gmail.com",
+      bio: "loremipsumloreamipsum",
+      linkedIn: "loremipsumloreamipsum",
+    },
+  ];
+
+  return (
+    <div>
+      <h1>ABOUT</h1>
+      <h2>The Game</h2>
+      <p>
+        This game is great and we're gonna knock it out of this park in the next
+        coming week
+      </p>
+      <h2>The Team</h2>
+      <div>
+        {team.map((teamMember) => (
+          <div key={teamMember.name}>
+            <h3>{teamMember.name}</h3>
+            <p>{teamMember.bio}</p>
+            <a href={`mailto:${teamMember.email}}`}>✉️</a>
+            <a href={`https://www.linkedin.com/in/${teamMember.linkedIn}`}>
+              👨🏽‍💼
+            </a>
+            <a href={`https://github.com/${teamMember.github}`}>🐈‍⬛</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default About;
