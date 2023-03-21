1eabf5a8e418fdeaffac9a69c7dd52e244f310e9
jon <jkwei.86@gmail.com>
2023-03-20 23:03:50 -0400

    implemented real time chat beta

--- pages/game/index.js
+++ pages/game/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Messages from "@/components/Messages/Messages";
 import supabase from "@/lib/supabase";
 
 const Game = () => {
@@ -13,14 +14,24 @@ const Game = () => {
     )
     .subscribe();
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const { content } = Object.fromEntries(new FormData(form));
+
+    form.reset();
+    const { data } = await supabase
+      .from("messages")
+      .insert({ content: content });
+  };
+
   return (
     <div>
-      <p>hi</p>
-      <form>
-        <div>
-          <label htmlFor="content">Message</label>
-          <textarea name="content" id="content"></textarea>
-        </div>
+      <Messages />
+      <form onSubmit={submitHandler}>
+        <label htmlFor="content">Message</label>
+        <textarea name="content" type="text"></textarea>
+        <button>Send</button>
       </form>
     </div>
   );
