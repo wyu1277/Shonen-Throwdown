e067f8509ee516034b616b60bfdbb6fdb4af9027
Kyle Wong <Kwong498@gmail.com>
2023-03-21 11:03:21 -0400

    test

--- pages/play/index.js
+++ pages/play/index.js
@@ -1,7 +1,9 @@
-export default function Home() {
-	return (
-		<>
-			<p>Peppermint Patties Home Page</p>
-		</>
-	);
-}
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
+	realtime: {
+		params: {
+			eventsPerSecond: 10
+		}
+	}
+});
