4c26f8f89477cc1f4ebc8b6a7e12297341408eaf
Kyle Wong <Kwong498@gmail.com>
2023-03-22 00:05:09 -0400

    testing get routes with new db schema

--- pages/collection/index.js
+++ pages/collection/index.js
@@ -1,89 +1,81 @@
 // import { Auth } from '@supabase/auth-ui-react';
 // import { ThemeSupa } from '@supabase/auth-ui-shared';
 //! INSTALL WHEN NEEDED
-import Modal from "@/components/collection/modal";
-import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
-import { useEffect, useState } from "react";
-import styles from "./Collection.module.css";
-import { motion } from "framer-motion";
+import Modal from '@/components/collection/modal';
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useEffect, useState } from 'react';
+import styles from './Collection.module.css';
+import { motion } from 'framer-motion';
 
 const Collection = () => {
-  const supabase = useSupabaseClient();
-  const user = useUser();
+	const supabase = useSupabaseClient();
+	const user = useUser();
 
-  const [data, setData] = useState();
-  const [searchInput, setSearchInput] = useState("");
-  const [selectedCard, setSelectedCard] = useState(null);
-  const [pageMessage, setPageMessage] = useState("Loading...");
+	const [data, setData] = useState();
+	const [searchInput, setSearchInput] = useState('');
+	const [selectedCard, setSelectedCard] = useState(null);
+	const [pageMessage, setPageMessage] = useState('Loading...');
 
-  useEffect(() => {
-    setTimeout(() => {
-      setPageMessage("There are no cards avalible");
-    }, 1000);
-    if (!user) {
-      const loadData = async () => {
-        const { data } = await supabase.from("cards").select("*");
-        setData(data);
-      };
-      loadData();
-    } else {
-      const loadData = async () => {
-        const { data } = await supabase.from("collections").select("*");
-        setData(data);
-        console.log("logged in collection", data);
-      };
-      loadData();
-    }
-  }, []);
+	useEffect(() => {
+		setTimeout(() => {
+			setPageMessage('There are no cards avalible');
+		}, 1000);
+		if (!user) {
+			const loadData = async () => {
+				const { data } = await supabase.from('cards').select('*');
+				setData(data);
+			};
+			loadData();
+		} else {
+			const loadData = async () => {
+				const { data } = await supabase.from('collections').select('*').eq('user', user.id);
+				setData(data);
+				console.log('logged in collection', data);
+			};
+			loadData();
+		}
+	}, []);
 
-  const filteredData =
-    data &&
-    data.filter((card) =>
-      card.name.toLowerCase().includes(searchInput.toLowerCase())
-    );
+	const filteredData = data && data.filter((card) => card.name.toLowerCase().includes(searchInput.toLowerCase()));
 
-  const handleCardClick = (card) => {
-    setSelectedCard(card);
-  };
+	const handleCardClick = (card) => {
+		setSelectedCard(card);
+	};
 
-  return (
-    <div className={styles.pageParent}>
-      <div className={styles.searchParent}>
-        <input
-          className={styles.searchBar}
-          type="text"
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          placeholder="Search by name..."
-        />
-      </div>
-      <div className={styles.cardParent}>
-        {filteredData !== undefined ? (
-          filteredData.map((card) => (
-            <motion.div
-              whileHover={{ scale: 1.5 }}
-              key={card.id}
-              onClick={() => handleCardClick(card)}
-              className={styles.card}
-            >
-              <img src={card.image} alt={card.name} className={styles.img} />
-            </motion.div>
-          ))
-        ) : (
-          <div className={styles.loading}>
-            <h1>{pageMessage}</h1>
-          </div>
-        )}
-      </div>
-      <div className={styles.modalParent}>
-        <Modal
-          open={selectedCard !== null}
-          card={selectedCard}
-          onClose={() => setSelectedCard(null)}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.pageParent}>
+			<div className={styles.searchParent}>
+				<input
+					className={styles.searchBar}
+					type="text"
+					value={searchInput}
+					onChange={(e) => setSearchInput(e.target.value)}
+					placeholder="Search by name..."
+				/>
+			</div>
+			<div className={styles.cardParent}>
+				{filteredData !== undefined ? (
+					filteredData.map((card) => (
+						<motion.div
+							whileHover={{ scale: 1.5 }}
+							key={card.id}
+							onClick={() => handleCardClick(card)}
+							className={styles.card}
+						>
+							<img src={card.image} alt={card.name} className={styles.img} />
+						</motion.div>
+					))
+				) : (
+					<div className={styles.loading}>
+						<h1>{pageMessage}</h1>
+					</div>
+				)}
+			</div>
+			<div className={styles.modalParent}>
+				<Modal open={selectedCard !== null} card={selectedCard} onClose={() => setSelectedCard(null)} />
+			</div>
+		</div>
+	);
 };
 
 export default Collection;
