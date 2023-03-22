b51eda6228f7da8b9a02782bd4fdfae99cf691a8
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-03-22 16:11:33 -0500

    added backdrop, and added triggers and functions

--- components/collection/modal.js
+++ components/collection/modal.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
-import styles from './Modal.module.css';
-import ReactDom from 'react-dom';
+import React, { useState } from "react";
+import styles from "./Modal.module.css";
+import ReactDom from "react-dom";
+import { motion } from "framer-motion";
 
-let Modal = ({ open, card, onClose }) => {
-	if (!open) return null;
-	return ReactDom.createPortal(
-		<div className={styles.card}>
-			<button onClick={onClose} className={styles.close}>
-				Close
-			</button>
-			<img src={card.image} alt={card.name} className={styles.img} />
-		</div>,
-		document.getElementById('portal')
-	);
+let Modal = (props) => {
+  console.log(props);
+  //   if (!open) return null;
+  //   return ReactDom.createPortal(
+  //     <motion.div className="backdrop">
+  //       <div
+  //         className={styles.card}
+  //         onClick={() => props.setShowModal(!props.showModal)}
+  //       >
+  //         <button className={styles.close}>Close</button>
+  //         <img src="s" alt="fe" className={styles.img} />
+  //       </div>
+  //     </motion.div>,
+  //     document.getElementById("portal")
+  //   );
+
+  return (
+    <motion.div className="backdrop">
+      <div
+        className={styles.card}
+        onClick={() => props.setShowModal(!props.showModal)}
+      >
+        <button className={styles.close}>Close</button>
+        <img
+          src={props.card.image}
+          alt={props.card.name}
+          className={styles.img}
+        />
+      </div>
+    </motion.div>
+  );
 };
 
 export default Modal;
--- styles/variants.js
+++ styles/variants.js
@@ -1,3 +1,8 @@
+export const backDrop = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+};
+
 const container = {
   initial: { x: -3000 },
   visible: { x: 0, transition: { duration: 1 } },
