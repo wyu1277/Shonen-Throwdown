4ba9372509b12933b35e89bc91bd8c8da780f0ed
jon <jkwei.86@gmail.com>
2023-03-23 02:18:11 -0400

    push for additional testing

--- components/GameRoom/GameRoom.js
+++ components/GameRoom/GameRoom.js
@@ -5,29 +5,32 @@ import { useUser } from "@supabase/auth-helpers-react";
 import { useSelector } from "react-redux";
 import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
-const GameRoom = (props) => {
-  const user = useUser();
+const GameRoom = ({ props }) => {
+  //const user = useUser();
   const router = useRouter();
   const [presence, setPresence] = useState();
   const [player1, setPlayer1] = useState();
   const [player2, setPlayer2] = useState();
   const [currentUser, setCurrentUser] = useState();
 
+  const user = useSelector((state) => state.user.user);
+
   console.log("this is props", props);
 
   useEffect(() => {
     const channel = supabase.channel("test", {
       config: {
-        presence: { player: props.id, username: props.username },
+        presence: { props },
       },
     });
     channel
       .on("presence", { event: "sync" }, () => {
         const state = channel.presenceState();
         setPresence(state);
+        console.log("this is presence", presence);
       })
       .on("presence", { event: "join" }, ({ newPresences }) => {
-        console.log(newPresences, "Has Joined");
+        console.log(newPresences.flat(), "Has Joined");
       })
       .on("presence", { event: "leave" }, ({ leftPresences }) => {
         console.log(leftPresences, "has left");
@@ -35,8 +38,7 @@ const GameRoom = (props) => {
       .subscribe(async (status) => {
         if (status === "SUBSCRIBED") {
           const status = await channel.track({
-            online_at: new Date().toISOString(),
-            player: { id: props.id, username: props.username },
+            presence: { props },
           });
           await channel.untrack();
         }
@@ -49,8 +51,6 @@ const GameRoom = (props) => {
     console.log("this is to track presence status", presence);
     router.push("http://localhost:3000/");
   };
-
-  console.log("this is presence state in game rooom##########", presence);
 
   return (
     <div>
--- pages/game/index.js
+++ pages/game/index.js
@@ -3,27 +3,17 @@ import Messages from "@/components/Messages/Messages";
 import supabase from "@/lib/supabase";
 import GameRoom from "@/components/GameRoom/GameRoom";
 import { useUser } from "@supabase/auth-helpers-react";
+import { useSelector } from "react-redux";
 
 const Game = () => {
   const user = useUser();
   const [currentUser, setCurrentUser] = useState();
-
-  useEffect(() => {
-    const getCurrentUser = async () => {
-      const { data } = await supabase
-        .from("users")
-        .select("*")
-        .eq("id", user.id)
-        .single();
-      setCurrentUser(data);
-    };
-    getCurrentUser();
-  }, []);
+  const publicUser = useSelector((state) => state.user.user);
 
   return (
     <div>
-      <GameRoom props={currentUser} />
-      <Messages props={currentUser} />
+      <GameRoom props={publicUser} />
+      <Messages props={publicUser} />
     </div>
   );
 };
