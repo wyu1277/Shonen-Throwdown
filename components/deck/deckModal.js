79a8c7d1e6913682c5e0c5ab29e6edb6509f83f7
Kyle Wong <Kwong498@gmail.com>
2023-03-23 17:48:04 -0400

    deck view for user is rendering

---
+++ components/deck/deckModal.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import styles from './DeckModal.module.css';
+import ReactDom from 'react-dom';
+import { motion } from 'framer-motion';
+import { useUser } from '@supabase/auth-helpers-react';
+
+let DeckModal = (props) => {
+	const user = useUser();
+	//   if (!open) return null;
+	//   return ReactDom.createPortal(
+	//     <motion.div className="backdrop">
+	//       <div
+	//         className={styles.card}
+	//         onClick={() => props.setShowModal(!props.showModal)}
+	//       >
+	//         <button className={styles.close}>Close</button>
+	//         <img src="s" alt="fe" className={styles.img} />
+	//       </div>
+	//     </motion.div>,
+	//     document.getElementById("portal")
+	//   );
+
+	return (
+		<motion.div className="backdrop">
+			<div className={styles.card} onClick={() => props.setShowModal(!props.showModal)}>
+				{user ? (
+					<button className={styles.removeButton} onClick={() => addToDeck(card)}>
+						Remove from Deck
+					</button>
+				) : null}
+				<button className={styles.close}>Close</button>
+				<img src={props.card.image} alt={props.card.name} className={styles.img} />
+			</div>
+		</motion.div>
+	);
+};
+
+export default DeckModal;
--- pages/deck/index.js
+++ pages/deck/index.js
@@ -1,5 +1,69 @@
-const index = () => {
-	return <div>DECK PAGE!!!!!</div>;
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import container from '../../styles/variants';
+import styles from './Deck.module.css';
+import DeckModal from '@/components/deck/deckModal';
+
+const Deck = () => {
+	const user = useUser();
+	const [data, setData] = useState();
+	const [selectedCard, setSelectedCard] = useState(null);
+	const [showModal, setShowModal] = useState(false);
+	const [pageMessage, setPageMessage] = useState('Loading...');
+	const supabase = useSupabaseClient();
+
+	useEffect(() => {
+		const getDeckCards = async () => {
+			const deckRow = await supabase.from('decks').select('*').eq('user_id', user.id);
+			console.log('Row from deck table', deckRow.data[0].card_ids);
+			const cards = await supabase.from('cards').select('*').in('id', deckRow.data[0].card_ids);
+			console.log('cards', cards.data);
+			setData(cards.data);
+		};
+		getDeckCards();
+	}, []);
+
+	const handleCardClick = (card) => {
+		setSelectedCard(card);
+	};
+
+	return (
+		<motion.div variants={container} initial="initial" animate="visible" exit="exit" className={styles.pageParent}>
+			<div className={styles.cardParent}>
+				{data !== undefined ? (
+					data.map((card) => (
+						<motion.div
+							whileHover={{ scale: 1.5 }}
+							key={card.id}
+							onClick={() => {
+								setShowModal(!showModal);
+								handleCardClick(card);
+							}}
+							className={styles.card}
+							//   whileTap={{ scale: 0.5, x: window.innerWidth / 2 }}
+						>
+							<img src={card.image} alt={card.name} className={styles.img} />
+						</motion.div>
+					))
+				) : (
+					<div className={styles.loading}>
+						<h1>{pageMessage}</h1>
+					</div>
+				)}
+			</div>
+			<div className={styles.modalParent}>
+				{showModal && (
+					<DeckModal
+						showModal={showModal}
+						setShowModal={setShowModal}
+						card={selectedCard}
+						onClose={() => setSelectedCard(null)}
+					/>
+				)}
+			</div>
+		</motion.div>
+	);
 };
 
-export default index;
+export default Deck;
