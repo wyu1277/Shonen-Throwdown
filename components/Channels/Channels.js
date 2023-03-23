3e63c22971f05c7174ec9238b2cc88b50347694a
jon <jkwei.86@gmail.com>
2023-03-23 16:57:27 -0400

    new branch for channels testing

---
+++ components/Channels/Channels.js
@@ -0,0 +1,48 @@
+import Game from "@/pages/game";
+import React, { useEffect, useState } from "react";
+import supabase from "../../lib/supabase";
+import { useRouter } from "next/router";
+
+const Channels = () => {
+  const [channels, setChannels] = useState([]);
+  const router = useRouter();
+
+  //useeffect to fetch all game IDs aka channels and set game ID to channel ID
+  useEffect(() => {
+    const getChannels = async () => {
+      const { data } = await supabase.from("game").select("*");
+      setChannels(data);
+      console.log(data);
+      console.log("this is data in channels", channels);
+    };
+    getChannels();
+  }, []);
+
+  const handleJoin = (e) => {
+    console.log(e.target.value);
+    //set channel to e.target.value
+    //ref to new game[id].js where channel = e.target.value
+    const gameId = e.target.value;
+    router.push("/game/" + gameId);
+  };
+
+  return (
+    <div>
+      Channels:
+      <h4>
+        <ul>
+          {channels.map((channel) => (
+            <li key={channel.id}>
+              <button onClick={handleJoin} value={channel.id}>
+                Join: {channel.id}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </h4>
+      {/*create room will cause trigger to create new game row and create route user to newly created game[id].js set player1 to user who created and wait for player 2*/}
+      <button>Create Room</button>
+    </div>
+  );
+};
+export default Channels;
---
+++ pages/game/[id].js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import Messages from "@/components/Messages/Messages";
+import supabase from "@/lib/supabase";
+import GameRoom from "@/components/GameRoom/GameRoom";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useSelector } from "react-redux";
+
+const Game = (gameId) => {
+  const user = useUser();
+  const publicUser = useSelector((state) => state.user.user);
+
+  return (
+    <div>
+      <GameRoom props={(publicUser, gameId)} />
+      <Messages props={publicUser} />
+    </div>
+  );
+};
+
+export default Game;
