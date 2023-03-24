2560a36ad40cf14eb61f47d8ffb048052d73cf50
jon <jkwei.86@gmail.com>
2023-03-23 21:08:23 -0400

    added files from previous commit

---
+++ pages/[game]/index.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import Messages from "@/components/Messages/Messages";
+import supabase from "@/lib/supabase";
+import GameRoom from "@/components/GameRoom/GameRoom";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useSelector } from "react-redux";
+import Channels from "@/components/Channels/Channels";
+
+const Game = () => {
+  const user = useUser();
+  const publicUser = useSelector((state) => state.user.user);
+
+  console.log(
+    "########################################################PUBIC USER###################",
+    user
+  );
+
+  console.log("pubic user", publicUser);
+
+  return (
+    <div>
+      <GameRoom props={publicUser} />
+      <Messages props={publicUser} />
+    </div>
+  );
+};
+
+export default Game;
---
+++ pages/lobby/index.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Messages from "@/components/Messages/Messages";
+import supabase from "@/lib/supabase";
+import GameRoom from "@/components/GameRoom/GameRoom";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useSelector } from "react-redux";
+import Channels from "@/components/Channels/Channels";
+
+const Lobby = () => {
+  const user = useUser();
+  const publicUser = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", user.id)
+        .single();
+
+      const publicUser = await getUser();
+      console.log("this is data", data);
+      console.log("public userrrrrrrrrrrrrrrrrrrrrr", publicUser);
+      return publicUser;
+    };
+    getUser();
+  }, []);
+
+  console.log("this is public", publicUser);
+
+  return (
+    <div>
+      <Messages props={publicUser} />
+      <Channels props={publicUser} />
+    </div>
+  );
+};
+
+export default Lobby;
