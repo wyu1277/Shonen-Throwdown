517712172ab31a4b8bfed7ab3fbf86286c078580
Kyle Wong <Kwong498@gmail.com>
2023-04-01 15:48:02 -0400

    end game logic moved to game component

--- components/Game/Player2HP.js
+++ components/Game/Player2HP.js
@@ -1,32 +1,28 @@
-import { motion } from "framer-motion";
-import { useSelector, useDispatch } from "react-redux";
-import Router from "next/router";
-import { useEffect } from "react";
-import { gameActions } from "@/store/slices/gameSlice";
-import { loadActions } from "@/store/slices/loadSlice";
+import { motion } from 'framer-motion';
+import { useSelector, useDispatch } from 'react-redux';
+import Router from 'next/router';
+import { useEffect } from 'react';
+import { gameActions } from '@/store/slices/gameSlice';
+import { loadActions } from '@/store/slices/loadSlice';
 
 const Player2HP = (props) => {
-  const dispatch = useDispatch();
-  console.log(props, "PLAYER2 PROPS");
-  // const router = useRouter();
-  const health = useSelector((state) => {
-    return state.game.player2HP;
-  });
+	const dispatch = useDispatch();
+	console.log(props, 'PLAYER2 PROPS');
+	// const router = useRouter();
+	const health = useSelector((state) => {
+		return state.game.player2HP;
+	});
 
-  useEffect(() => {
-    health <= 0 && dispatch(gameActions.endGame(true));
-    health <= 0 && dispatch(loadActions.setLoading(true));
-  }, [health]);
+	useEffect(() => {
+		health <= 0 && dispatch(gameActions.endGame(true));
+		health <= 0 && dispatch(loadActions.setLoading(true));
+	}, [health]);
 
-  return (
-    <motion.div
-      initial={{ scale: 0, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      className="player2-hp"
-    >
-      {props?.opp} HP: {health}
-    </motion.div>
-  );
+	return (
+		<motion.div initial={{ scale: 0, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} className="player2-hp">
+			{props?.opp} HP: {health}
+		</motion.div>
+	);
 };
 
 export default Player2HP;
