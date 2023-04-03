2648b5fef988bc8328fd27c884e83eb015125257
jon <jkwei.86@gmail.com>
2023-04-03 13:27:33 -0400

    about page styling

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 // import TeamMate from "@/components/teammate";
 import { motion } from "framer-motion";
@@ -5,6 +6,23 @@ import container from "@/styles/variants";
 import styles from "./About.module.css";
 
 const About = () => {
+  const buttons = document.querySelectorAll("[data-carousel-button]");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+      const slides = button
+        .closest("[data-carousel]")
+        .querySelector("[data-slides]");
+      const activeSlide = slides.querySelector("[data-active]");
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+      if (newIndex < 0) newIndex = slides.children.length - 1;
+      if (newIndex >= slides.children.length) newIndex = 0;
+
+      slides.children[newIndex].dataset.active = true;
+      delete activeSlide.dataset.active;
+    });
+  });
   return (
     <motion.div
       variants={container}
@@ -18,39 +36,69 @@ const About = () => {
           src="https://i.imgur.com/7COqc6b.png"
           alt="Pick a developer, any developer!"
         ></img>
-        <h2>The Game</h2>
-        <p>
-          This game is great and we're gonna knock it out of this park in the
-          next coming week
-        </p>
-        <h2>The Team</h2>
-        <div className={styles.kylesCard}>
-          <h3>Kyle Wong</h3>
-          <p>loremipsumloreamipsum</p>
-          <a href={`mailto:kwong498@gmail.com}`}>✉️</a>
-          <a href={`https://www.linkedin.com/in/kylematthewwong`}>👨🏽‍💼</a>
-          <a href={`https://github.com/Kywongster`}>🐈‍⬛</a>
-        </div>
-        <div className={styles.victorsCard}>
-          <h3>Victor Flores</h3>
-          <p>loremipsumloreamipsum</p>
-          <a href={`mailto:thelegendofvictor@gmail.com}`}>✉️</a>
-          <a href={`https://www.linkedin.com/in/mikloflores`}>👨🏽‍💼</a>
-          <a href={`https://github.com/Miklo1775`}>🐈‍⬛</a>
-        </div>
-        <div className={styles.wilsonsCard}>
-          <h3>Wilson Yu</h3>
-          <p>loremipsumloreamipsum</p>
-          <a href={`mailto:wyu1277@gmail.com}`}>✉️</a>
-          <a href={`https://www.linkedin.com/in/whydesigns`}>👨🏽‍💼</a>
-          <a href={`https://github.com/github`}>🐈‍⬛</a>
-        </div>
-        <div className={styles.jonsCard}>
-          <h3>Jonathan Wei</h3>
-          <p>loremipsumloreamipsum</p>
-          <a href={`mailto:jkwei.86@gmail.com}`}>✉️</a>
-          <a href={`https://www.linkedin.com/in/linkedIn}`}>👨🏽‍💼</a>
-          <a href={`https://github.com/github}`}>🐈‍⬛</a>
+        <div className={styles.carousel} data-carousel>
+          <button
+            className={styles.carouselButtonPrev}
+            data-carousel-button="prev"
+          >
+            &#11160;
+          </button>
+          <div className={styles.dataSlides} data-slides>
+            <div className={styles.slide} data-active>
+              <img
+                className={styles.slideImg}
+                src="https://i.imgur.com/GDBpXpC.png"
+                alt="developer-card-kyle"
+              ></img>
+              <h3>Kyle Wong</h3>
+              <p>loremipsumloreamipsum</p>
+              <a href={`mailto:kwong498@gmail.com}`}>✉️</a>
+              <a href={`https://www.linkedin.com/in/kylematthewwong`}>👨🏽‍💼</a>
+              <a href={`https://github.com/Kywongster`}>🐈‍⬛</a>
+            </div>
+            <div className={styles.slide}>
+              <img
+                className={styles.slideImg}
+                src="https://i.imgur.com/u7SeDyz.png"
+                alt="developer-card-victor"
+              ></img>
+              <h3>Victor Flores</h3>
+              <p>loremipsumloreamipsum</p>
+              <a href={`mailto:thelegendofvictor@gmail.com}`}>✉️</a>
+              <a href={`https://www.linkedin.com/in/mikloflores`}>👨🏽‍💼</a>
+              <a href={`https://github.com/Miklo1775`}>🐈‍⬛</a>
+            </div>
+            <div className={styles.slide}>
+              <img
+                className={styles.slideImg}
+                src="https://i.imgur.com/nmhfFBZ.png"
+                alt="developer-card-wilson"
+              ></img>
+              <h3>Wilson Yu</h3>
+              <p>loremipsumloreamipsum</p>
+              <a href={`mailto:wyu1277@gmail.com}`}>✉️</a>
+              <a href={`https://www.linkedin.com/in/whydesigns`}>👨🏽‍💼</a>
+              <a href={`https://github.com/github`}>🐈‍⬛</a>
+            </div>
+            <div className={styles.slide}>
+              <img
+                className={styles.slideImg}
+                src="https://i.imgur.com/u7SeDyz.png"
+                alt="developer-card-jon"
+              ></img>
+              <h3>Jonathan Wei</h3>
+              <p>loremipsumloreamipsum</p>
+              <a href={`mailto:jkwei.86@gmail.com}`}>✉️</a>
+              <a href={`https://www.linkedin.com/in/linkedIn}`}>👨🏽‍💼</a>
+              <a href={`https://github.com/github}`}>🐈‍⬛</a>
+            </div>
+            <button
+              className={styles.carouselButtonNext}
+              data-carousel-button="next"
+            >
+              &#11162;
+            </button>
+          </div>
         </div>
       </div>
     </motion.div>
