936f8dd7d36b7610ac70610e3113441a617bbc97
Kyle Wong <Kwong498@gmail.com>
2023-04-03 14:37:20 -0400

    added cursor hover/effects on home, nav, and market, fixed loading for home

--- pages/index.js
+++ pages/index.js
@@ -40,62 +40,65 @@ const Home = ({ user }) => {
 	useEffect(() => {
 		// console.log("loading");
 
+		if (shouldReload) {
+			window.location.reload();
+		}
+		if (user) {
+			dispatch(searchUser(user.id));
+			dispatch(fetchDeckCards(user.id));
+		}
+		// console.log(userData, "USERDATA");
+	}, [userInfo]);
 
-    if (shouldReload) {
-      window.location.reload();
-    }
-    if (user) {
-      dispatch(searchUser(user.id));
-      dispatch(fetchDeckCards(user.id));
-    }
-    // console.log(userData, "USERDATA");
-  }, [userInfo]);
-
-  if (user && userData?.username === null) {
-    router.push("/login/setup-account");
-  }
-  return (
-    <motion.div
-      variants={container}
-      initial="initial"
-      animate="visible"
-      exit="exit"
-    >
-      {loadState ? (
-        <div>loading</div>
-      ) : (
-        <div className={styles.homepageContainer}>
-          <div className={styles.imgContainer}>
-            <Image className={styles.img} src={shonenthrowdown}/>
-            <button className={`${roboto.className} ${styles.button}`} onClick={()=>router.push('/lobby')}>PLAY NOW &#62;</button>
-          </div>
-          <div className={styles.section}>
-            <img className={styles.sectionImg} src='http://rubberslug.s3.amazonaws.com/user/3/262b639333041368802c7e7c7bbe5ff/fdoxmylygp_o.jpg'/>
-            <div className={styles.sectionInfo}>
-              <div className= {styles.sectionTextContainer}>
-                <h2>Featured Card</h2>
-                <h1>Ichigo Kurosaki</h1>
-                <button className={roboto.className} onClick={()=> router.push('/market')}>PULL NOW &#62;</button>
-              </div>
-              <img className={styles.cardImg} src ='https://i.imgur.com/SfS6xsj.png'/>
-            </div>
-          </div>
-          <div className={styles.section}>
-            <img className={styles.sectionImg} src='https://images.pexels.com/photos/172277/pexels-photo-172277.jpeg'/>
-            <div className={styles.sectionInfo}>
-              <img className={styles.cardImg2} src ='https://i.imgur.com/1RbpkCe.png'/>
-              <div className= {styles.sectionTextContainer}>
-                <h2>EASY TO LEARN, FUN TO PLAY!</h2>
-                <h1>BEGIN YOUR ADVENTURE</h1>
-                <h1>WITH OUR HOW TO PLAY!</h1>
-                <button className={roboto.className} onClick={()=> router.push('/how-to-play')}>LEARN MORE &#62;</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </motion.div>
-  );
+	if (user && userData?.username === null) {
+		router.push('/login/setup-account');
+	}
+	return (
+		<motion.div variants={container} initial="initial" animate="visible" exit="exit" className={styles.pageContainer}>
+			{loadState ? (
+				<div className={styles.loading}>LOADING...</div>
+			) : (
+				<div className={styles.homepageContainer}>
+					<div className={styles.imgContainer}>
+						<Image className={styles.img} src={shonenthrowdown} />
+						<button className={`${roboto.className} ${styles.button}`} onClick={() => router.push('/lobby')}>
+							PLAY NOW &#62;
+						</button>
+					</div>
+					<div className={styles.section}>
+						<img
+							className={styles.sectionImg}
+							src="http://rubberslug.s3.amazonaws.com/user/3/262b639333041368802c7e7c7bbe5ff/fdoxmylygp_o.jpg"
+						/>
+						<div className={styles.sectionInfo}>
+							<div className={styles.sectionTextContainer}>
+								<h2>Featured Card</h2>
+								<h1>Ichigo Kurosaki</h1>
+								<button className={roboto.className} onClick={() => router.push('/market')}>
+									PULL NOW &#62;
+								</button>
+							</div>
+							<img className={styles.cardImg} src="https://i.imgur.com/SfS6xsj.png" />
+						</div>
+					</div>
+					<div className={styles.section}>
+						<img className={styles.sectionImg} src="https://images.pexels.com/photos/172277/pexels-photo-172277.jpeg" />
+						<div className={styles.sectionInfo}>
+							<img className={styles.cardImg2} src="https://i.imgur.com/1RbpkCe.png" />
+							<div className={styles.sectionTextContainer}>
+								<h2>EASY TO LEARN, FUN TO PLAY!</h2>
+								<h1>BEGIN YOUR ADVENTURE</h1>
+								<h1>WITH OUR HOW TO PLAY!</h1>
+								<button className={roboto.className} onClick={() => router.push('/how-to-play')}>
+									LEARN MORE &#62;
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</motion.div>
+	);
 };
 
 export const getServerSideProps = async (context) => {
