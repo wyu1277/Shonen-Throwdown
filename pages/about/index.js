bb94c0f7f563b1f61169f29926a98c41c6556193
Kyle Wong <Kwong498@gmail.com>
2023-04-03 23:25:11 -0400

    set max-width for about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,164 +1,107 @@
-import React, { useEffect } from "react";
-import { motion } from "framer-motion";
-import container from "@/styles/variants";
-import styles from "./About.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
-import {
-  faAnglesLeft,
-  faAnglesRight,
-  faSquareEnvelope,
-} from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
+import container from '@/styles/variants';
+import styles from './About.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import { faAnglesLeft, faAnglesRight, faSquareEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
-  useEffect(() => {
-    const buttons = document.querySelectorAll("[data-carousel-button]");
+	useEffect(() => {
+		const buttons = document.querySelectorAll('[data-carousel-button]');
 
-    buttons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-        const slides = button
-          .closest("[data-carousel]")
-          .querySelector("[data-slides]");
-        const activeSlide = slides.querySelector("[data-active]");
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        if (newIndex < 0) newIndex = slides.children.length - 1;
-        if (newIndex >= slides.children.length) newIndex = 0;
+		buttons.forEach((button) => {
+			button.addEventListener('click', () => {
+				const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+				const slides = button.closest('[data-carousel]').querySelector('[data-slides]');
+				const activeSlide = slides.querySelector('[data-active]');
+				let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+				if (newIndex < 0) newIndex = slides.children.length - 1;
+				if (newIndex >= slides.children.length) newIndex = 0;
 
-        slides.children[newIndex].dataset.active = true;
-        delete activeSlide.dataset.active;
-      });
-    });
-  });
+				slides.children[newIndex].dataset.active = true;
+				delete activeSlide.dataset.active;
+			});
+		});
+	});
 
-  return (
-    <motion.div
-      variants={container}
-      initial="initial"
-      animate="visible"
-      exit="exit"
-    >
-      <div className={styles.container}>
-        <img
-          className={styles.banner}
-          src="https://i.imgur.com/7COqc6b.png"
-          alt="Pick a developer, any developer!"
-        ></img>
-        <div className={styles.carousel} data-carousel>
-          <button data-carousel-button="prev">
-            <FontAwesomeIcon
-              icon={faAnglesLeft}
-              className={styles.carouselButtonPrev}
-            />
-          </button>
-          <ul className={styles.dataSlides} data-slides>
-            <li className={styles.slide} data-active>
-              <img
-                className={styles.slideImg}
-                src="https://i.imgur.com/viDgPzH.png"
-                alt="developer-card-kyle"
-              ></img>
-              <div className={styles.cardButtons}>
-                <a href={`mailto:kwong498@gmail.com`}>
-                  <FontAwesomeIcon
-                    icon={faSquareEnvelope}
-                    className={styles.icons}
-                  />
-                </a>
-                <a href={`https://www.linkedin.com/in/kylematthewwong`}>
-                  <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
-                </a>
-                <a href={`https://github.com/Kywongster`}>
-                  <FontAwesomeIcon
-                    icon={faSquareGithub}
-                    className={styles.icons}
-                  />
-                </a>
-              </div>
-            </li>
-            <li className={styles.slide}>
-              <img
-                className={styles.slideImg}
-                src="https://i.imgur.com/mbhsWYT.png"
-                alt="developer-card-victor"
-              ></img>
-              <div className={styles.cardButtons}>
-                <a href={`mailto:thelegendofvictor@gmail.com`}>
-                  <FontAwesomeIcon
-                    icon={faSquareEnvelope}
-                    className={styles.icons}
-                  />
-                </a>
-                <a href={`https://www.linkedin.com/in/mikloflores`}>
-                  <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
-                </a>
-                <a href={`https://github.com/Miklo1775`}>
-                  <FontAwesomeIcon
-                    icon={faSquareGithub}
-                    className={styles.icons}
-                  />
-                </a>
-              </div>
-            </li>
-            <li className={styles.slide}>
-              <img
-                className={styles.slideImg}
-                src="https://i.imgur.com/pQneKpj.png"
-                alt="developer-card-wilson"
-              ></img>
-              <div className={styles.cardButtons}>
-                <a href={`mailto:wyu1277@gmail.com`}>
-                  <FontAwesomeIcon
-                    icon={faSquareEnvelope}
-                    className={styles.icons}
-                  />
-                </a>
-                <a href={`https://www.linkedin.com/in/whydesigns`}>
-                  <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
-                </a>
-                <a href={`https://github.com/wyu1277`}>
-                  <FontAwesomeIcon
-                    icon={faSquareGithub}
-                    className={styles.icons}
-                  />
-                </a>
-              </div>
-            </li>
-            <li className={styles.slide}>
-              <img
-                className={styles.slideImg}
-                src="https://i.imgur.com/TFR01Al.png"
-                alt="developer-card-jon"
-              ></img>
-              <div className={styles.cardButtons}>
-                <a href={`mailto:jkwei.86@gmail.com`}>
-                  <FontAwesomeIcon
-                    icon={faSquareEnvelope}
-                    className={styles.icons}
-                  />
-                </a>
-                <a href={`https://www.linkedin.com/in/wei-jon/`}>
-                  <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
-                </a>
-                <a href={`https://github.com/JonWei86`}>
-                  <FontAwesomeIcon
-                    icon={faSquareGithub}
-                    className={styles.icons}
-                  />
-                </a>
-              </div>
-            </li>
-            <button data-carousel-button="next">
-              <FontAwesomeIcon
-                icon={faAnglesRight}
-                className={styles.carouselButtonNext}
-              />
-            </button>
-          </ul>
-        </div>
-      </div>
-    </motion.div>
-  );
+	return (
+		<motion.div variants={container} initial="initial" animate="visible" exit="exit" className={styles.aboutParent}>
+			<div className={styles.container}>
+				<img
+					className={styles.banner}
+					src="https://i.imgur.com/7COqc6b.png"
+					alt="Pick a developer, any developer!"
+				></img>
+				<div className={styles.carousel} data-carousel>
+					<button data-carousel-button="prev">
+						<FontAwesomeIcon icon={faAnglesLeft} className={styles.carouselButtonPrev} />
+					</button>
+					<ul className={styles.dataSlides} data-slides>
+						<li className={styles.slide} data-active>
+							<img className={styles.slideImg} src="https://i.imgur.com/viDgPzH.png" alt="developer-card-kyle"></img>
+							<div className={styles.cardButtons}>
+								<a href={`mailto:kwong498@gmail.com`}>
+									<FontAwesomeIcon icon={faSquareEnvelope} className={styles.icons} />
+								</a>
+								<a href={`https://www.linkedin.com/in/kylematthewwong`}>
+									<FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
+								</a>
+								<a href={`https://github.com/Kywongster`}>
+									<FontAwesomeIcon icon={faSquareGithub} className={styles.icons} />
+								</a>
+							</div>
+						</li>
+						<li className={styles.slide}>
+							<img className={styles.slideImg} src="https://i.imgur.com/mbhsWYT.png" alt="developer-card-victor"></img>
+							<div className={styles.cardButtons}>
+								<a href={`mailto:thelegendofvictor@gmail.com`}>
+									<FontAwesomeIcon icon={faSquareEnvelope} className={styles.icons} />
+								</a>
+								<a href={`https://www.linkedin.com/in/mikloflores`}>
+									<FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
+								</a>
+								<a href={`https://github.com/Miklo1775`}>
+									<FontAwesomeIcon icon={faSquareGithub} className={styles.icons} />
+								</a>
+							</div>
+						</li>
+						<li className={styles.slide}>
+							<img className={styles.slideImg} src="https://i.imgur.com/pQneKpj.png" alt="developer-card-wilson"></img>
+							<div className={styles.cardButtons}>
+								<a href={`mailto:wyu1277@gmail.com`}>
+									<FontAwesomeIcon icon={faSquareEnvelope} className={styles.icons} />
+								</a>
+								<a href={`https://www.linkedin.com/in/whydesigns`}>
+									<FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
+								</a>
+								<a href={`https://github.com/wyu1277`}>
+									<FontAwesomeIcon icon={faSquareGithub} className={styles.icons} />
+								</a>
+							</div>
+						</li>
+						<li className={styles.slide}>
+							<img className={styles.slideImg} src="https://i.imgur.com/TFR01Al.png" alt="developer-card-jon"></img>
+							<div className={styles.cardButtons}>
+								<a href={`mailto:jkwei.86@gmail.com`}>
+									<FontAwesomeIcon icon={faSquareEnvelope} className={styles.icons} />
+								</a>
+								<a href={`https://www.linkedin.com/in/wei-jon/`}>
+									<FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
+								</a>
+								<a href={`https://github.com/JonWei86`}>
+									<FontAwesomeIcon icon={faSquareGithub} className={styles.icons} />
+								</a>
+							</div>
+						</li>
+						<button data-carousel-button="next">
+							<FontAwesomeIcon icon={faAnglesRight} className={styles.carouselButtonNext} />
+						</button>
+					</ul>
+				</div>
+			</div>
+		</motion.div>
+	);
 };
 
 export default About;
