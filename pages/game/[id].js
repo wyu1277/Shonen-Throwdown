aaa20a5c36087c61b4adf7a7a40acac0d5972a4d
Kyle Wong <Kwong498@gmail.com>
2023-04-03 20:06:05 -0400

    added max view width for entire site

--- pages/game/[id].js
+++ pages/game/[id].js
@@ -1,50 +1,51 @@
-"use client";
-import React, { useEffect, useState, useContext } from "react";
-import Messages from "@/components/Messages/Messages";
-import { supabase } from "@/lib/supabase";
-import GameRoom from "@/components/GameRoom/GameRoom";
-import { useUser } from "@supabase/auth-helpers-react";
-import { useSelector } from "react-redux";
-import Channels from "@/components/Channels/Channels";
-import { GlobalContext } from "@/lib/GlobalContext";
-import GameComponent from "@/components/Game/GameComponent";
-import Loading from "@/components/Game/Loading";
-import EndModal from "@/components/Game/EndModal";
-import Throwaway from "@/components/Game/Throwaway";
+'use client';
+import React, { useEffect, useState, useContext } from 'react';
+import Messages from '@/components/Messages/Messages';
+import { supabase } from '@/lib/supabase';
+import GameRoom from '@/components/GameRoom/GameRoom';
+import { useUser } from '@supabase/auth-helpers-react';
+import { useSelector } from 'react-redux';
+import Channels from '@/components/Channels/Channels';
+import { GlobalContext } from '@/lib/GlobalContext';
+import GameComponent from '@/components/Game/GameComponent';
+import Loading from '@/components/Game/Loading';
+import EndModal from '@/components/Game/EndModal';
+import Throwaway from '@/components/Game/Throwaway';
+import styles from './game.module.css';
 
 const Game = (props) => {
-  const authUser = useUser();
-  const publicUser = useSelector((state) => state.user.user);
-  const [conUser, setConUser] = useState(null);
-  //const [conDeck, setConDeck] = useState(null)
-  const loading = useSelector((state) => {
-    return state.load;
-  });
+	const authUser = useUser();
+	const publicUser = useSelector((state) => state.user.user);
+	const [conUser, setConUser] = useState(null);
+	//const [conDeck, setConDeck] = useState(null)
+	const loading = useSelector((state) => {
+		return state.load;
+	});
 
-  const user = useSelector((state) => {
-    return state.user.user;
-  });
+	const user = useSelector((state) => {
+		return state.user.user;
+	});
 
-  const userDeck = useSelector((state) => {
-    return state.deck;
-  });
+	const userDeck = useSelector((state) => {
+		return state.deck;
+	});
 
-  const ended = useSelector((state) => {
-    return state.game.ended;
-  });
+	const ended = useSelector((state) => {
+		return state.game.ended;
+	});
 
-  return (
-    <div>
-      <GlobalContext.Provider value={publicUser}>
-        {/* <GameRoom props={publicUser} /> */}
-        {loading && !ended && <Loading />}
-        {!loading && <GameComponent user={user} userDeck={userDeck} />}
-        {ended && <EndModal />}
-        <Throwaway />
-        <Messages props={publicUser} />
-      </GlobalContext.Provider>
-    </div>
-  );
+	return (
+		<div className={styles.gameParent}>
+			<GlobalContext.Provider value={publicUser}>
+				{/* <GameRoom props={publicUser} /> */}
+				{loading && !ended && <Loading />}
+				{!loading && <GameComponent user={user} userDeck={userDeck} />}
+				{ended && <EndModal />}
+				<Throwaway />
+				<Messages props={publicUser} />
+			</GlobalContext.Provider>
+		</div>
+	);
 };
 
 export default Game;
